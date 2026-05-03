ba96ec74b20c7981d571894cf0375745005d6d8e
Keegan Wilgermein <arbour-dutiful.7o@icloud.com>
2026-05-03 21:10:42 +1200

    buffer size range and sample rate range

--- src/data_types.rs
+++ src/data_types.rs
@@ -1,6 +1,7 @@
 //! # Data types
 
 // ---- Imports ------------
+use std::ops::RangeInclusive;
 
 // ---- Enums ------------
 /// Input or output device selector
@@ -9,4 +10,43 @@ pub enum Scope {
     Input,
     /// Output devices
     Output,
+}
+
+// ----- Structs ------------
+/// A range of sizes supported by a device to be used as a buffer size
+pub struct BufferFrameSizeRange {
+    min: u32,
+    max: u32,
+}
+
+impl BufferFrameSizeRange {
+    /// Returns all values between `min` and `max` that are a power of 2
+    pub fn valid_sizes(&self) -> Vec<u32> {
+        let mut size = self.min.next_power_of_two();
+        let mut sizes = Vec::new();
+        while size <= self.max {
+            sizes.push(size);
+            size *= 2;
+        }
+        sizes
+    }
+}
+
+pub struct SampleRateRange {
+    min: f64,
+    max: f64,
+}
+
+impl SampleRateRange {
+    pub fn min(&self) -> f64 {
+        self.min
+    }
+
+    pub fn max(&self) -> f64 {
+        self.max
+    }
+
+    pub fn as_range(&self) -> RangeInclusive<f64> {
+        self.min..=self.max
+    }
 }
