99ee8cec7c203194635c2eba7cd4abed5f48e576
Keegan Wilgermein <arbour-dutiful.7o@icloud.com>
2026-05-04 09:37:36 +1200

    fixed indent on property constants

--- src/property.rs
+++ src/property.rs
@@ -250,333 +250,348 @@ const fn address(
 
 /// Human readable name of the device
 pub const DEVICE_NAME: Property<String, Device, ReadOnly, Silent> =
-    Property::new(
-        address(
-            kAudioObjectPropertyName,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_string,
-        None,
-    );
+Property::new(
+    address(
+        kAudioObjectPropertyName,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_string,
+    None,
+);
 
 /// Persistent unique identifier for the device
 /// 
 /// Not the same as the device id
 pub const DEVICE_UID: Property<String, Device, ReadOnly, Silent> =
-    Property::new(
-        address(
-            kAudioDevicePropertyDeviceUID,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_string,
-        None,
-    );
+Property::new(
+    address(
+        kAudioDevicePropertyDeviceUID,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_string,
+    None,
+);
 
 /// Whether the device is still alive and connected
 pub const DEVICE_IS_ALIVE: Property<bool, Device, ReadOnly, Listenable> =
-    Property::new(
-        address(
-            kAudioDevicePropertyDeviceIsAlive,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_bool,
-        None,
-    );
+Property::new(
+    address(
+        kAudioDevicePropertyDeviceIsAlive,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_bool,
+    None,
+);
 
 /// Whether the device is currently running I/O
 pub const DEVICE_IS_RUNNING: Property<bool, Device, ReadOnly, Listenable> =
-    Property::new(
-        address(
-            kAudioDevicePropertyDeviceIsRunning,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_bool,
-        None,
-    );
+Property::new(
+    address(
+        kAudioDevicePropertyDeviceIsRunning,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_bool,
+    None,
+);
 
 /// The current nominal sample rate of the device
 pub const DEVICE_NOMINAL_SAMPLE_RATE: Property<f64, Device, ReadWrite, Listenable> =
-    Property::new(
-        address(
-            kAudioDevicePropertyNominalSampleRate,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_f64,
-        Some(encode_f64),
-    );
+Property::new(
+    address(
+        kAudioDevicePropertyNominalSampleRate,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_f64,
+    Some(encode_f64),
+);
 
 /// All sample rates supported by the device
 pub(crate) const DEVICE_AVAILABLE_SAMPLE_RATES: Property<Vec<SampleRateRange>, Device, ReadOnly, Silent> =
-    Property::new(
-        address(
-            kAudioDevicePropertyAvailableNominalSampleRates,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_vec_sample_rate_range,
-        None,
-    );
+Property::new(
+    address(
+        kAudioDevicePropertyAvailableNominalSampleRates,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_vec_sample_rate_range,
+    None,
+);
 
 /// The number of frames in the I/O buffer
 pub const DEVICE_BUFFER_FRAME_SIZE: Property<u32, Device, ReadWrite, Listenable> =
-    Property::new(
-        address(
-            kAudioDevicePropertyBufferFrameSize,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_u32,
-        Some(encode_u32)
-    );
+Property::new(
+    address(
+        kAudioDevicePropertyBufferFrameSize,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_u32,
+    Some(encode_u32)
+);
 
 /// The valid range of buffer frame sizes for the device
 pub(crate) const DEVICE_BUFFER_FRAME_SIZE_RANGE: Property<BufferFrameSizeRange, Device, ReadOnly, Silent> =
-    Property::new(
-        address(
-            kAudioDevicePropertyBufferFrameSizeRange,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_buffer_size_range,
-        None,
-    );
+Property::new(
+    address(
+        kAudioDevicePropertyBufferFrameSizeRange,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_buffer_size_range,
+    None,
+);
 
 /// Input latency of the device in frames
 pub const DEVICE_INPUT_LATENCY: Property<u32, Device, ReadOnly, Silent> =
-    Property::new(
-        address(
-            kAudioDevicePropertyLatency,
-            kAudioObjectPropertyScopeInput
-        ),
-        read_u32,
-        None,
-    );
+Property::new(
+    address(
+        kAudioDevicePropertyLatency,
+        kAudioObjectPropertyScopeInput
+    ),
+    read_u32,
+    None,
+);
 
 /// Output latency of the device in frames
 pub const DEVICE_OUTPUT_LATENCY: Property<u32, Device, ReadOnly, Silent> =
-    Property::new(
-        address(
-            kAudioDevicePropertyLatency,
-            kAudioObjectPropertyScopeOutput
-        ),
-        read_u32,
-        None
-    );
+Property::new(
+    address(
+        kAudioDevicePropertyLatency,
+        kAudioObjectPropertyScopeOutput
+    ),
+    read_u32,
+    None
+);
 
 /// All input streams on the device
 pub(crate) const DEVICE_INPUT_STREAMS: Property<Vec<AudioObjectID>, Device, ReadOnly, Listenable> =
-    Property::new(
-        address(
-            kAudioDevicePropertyStreams,
-            kAudioObjectPropertyScopeInput
-        ),
-        read_vec_audio_object_id,
-        None,
-    );
+Property::new(
+    address(
+        kAudioDevicePropertyStreams,
+        kAudioObjectPropertyScopeInput
+    ),
+    read_vec_audio_object_id,
+    None,
+);
 
 /// All output streams on the device
 pub(crate) const DEVICE_OUTPUT_STREAMS: Property<Vec<AudioObjectID>, Device, ReadOnly, Listenable> =
-    Property::new(
-        address(
-            kAudioDevicePropertyStreams,
-            kAudioObjectPropertyScopeOutput
-        ),
-        read_vec_audio_object_id,
-        None,
-    );
+Property::new(
+    address(
+        kAudioDevicePropertyStreams,
+        kAudioObjectPropertyScopeOutput
+    ),
+    read_vec_audio_object_id,
+    None,
+);
 
 pub const DEVICE_HOG_MODE: Property<i32, Device, ReadWrite, Listenable> =
-    Property::new(
-        address(
-            kAudioDevicePropertyHogMode,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_i32,
-        Some(encode_i32)
-    );
+Property::new(
+    address(
+        kAudioDevicePropertyHogMode,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_i32,
+    Some(encode_i32)
+);
 
 // ---- Stream constants ----
 
 /// Human readable name of the stream
 pub const STREAM_NAME: Property<String, Stream, ReadOnly, Silent> =
-    Property::new(
-        address(
-            kAudioObjectPropertyName,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_string,
-        None,
-    );
+Property::new(
+    address(
+        kAudioObjectPropertyName,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_string,
+    None,
+);
 
 /// Whether the stream is currently active
 pub const STREAM_IS_ACTIVE: Property<bool, Stream, ReadOnly, Listenable> =
-    Property::new(
-        address(
-            kAudioStreamPropertyIsActive,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_bool,
-        None,
-    );
+Property::new(
+    address(
+        kAudioStreamPropertyIsActive,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_bool,
+    None,
+);
 
 /// Direction of the stream — 0 for output, 1 for input
 pub const STREAM_DIRECTION: Property<u32, Stream, ReadOnly, Silent> =
-    Property::new(
-        address(
-            kAudioStreamPropertyDirection,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_u32,
-        None,
-    );
+Property::new(
+    address(
+        kAudioStreamPropertyDirection,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_u32,
+    None,
+);
 
 /// The virtual format of the stream as presented to the client
 pub(crate) const STREAM_VIRTUAL_FORMAT: Property<StreamDescription, Stream, ReadWrite, Listenable> =
-    Property::new(
-        address(
-            kAudioStreamPropertyVirtualFormat,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_stream_description,
-        Some(encode_stream_description)
-    );
+Property::new(
+    address(
+        kAudioStreamPropertyVirtualFormat,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_stream_description,
+    Some(encode_stream_description)
+);
 
 /// The physical format of the stream as presented to the hardware
 pub(crate) const STREAM_PHYSICAL_FORMAT: Property<StreamDescription, Stream, ReadWrite, Listenable> =
-    Property::new(
-        address(
-            kAudioStreamPropertyPhysicalFormat,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_stream_description,
-        Some(encode_stream_description)
-    );
+Property::new(
+    address(
+        kAudioStreamPropertyPhysicalFormat,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_stream_description,
+    Some(encode_stream_description)
+);
 
 /// Latency of the stream in frames
 pub const STREAM_LATENCY: Property<u32, Stream, ReadOnly, Silent> =
-    Property::new(
-        address(
-            kAudioStreamPropertyLatency,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_u32,
-        None,
-    );
+Property::new(
+    address(
+        kAudioStreamPropertyLatency,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_u32,
+    None,
+);
 
 // ---- System constants ----
 
 /// Human readable name of the system object
 pub const SYSTEM_NAME: Property<String, System, ReadOnly, Silent> =
-    Property::new(
-        address(
-            kAudioObjectPropertyName,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_string,
-        None,
-    );
+Property::new(
+    address(
+        kAudioObjectPropertyName,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_string,
+    None,
+);
 
 /// All devices currently known to the HAL
 pub(crate) const SYSTEM_DEVICES: Property<Vec<AudioObjectID>, System, ReadOnly, Listenable> =
-    Property::new(
-        address(
-            kAudioHardwarePropertyDevices,
-            kAudioObjectPropertyScopeGlobal
-        ), read_vec_audio_object_id,
-        None,
-    );
+Property::new(
+    address(
+        kAudioHardwarePropertyDevices,
+        kAudioObjectPropertyScopeGlobal
+    ), read_vec_audio_object_id,
+    None,
+);
 
 /// The current default input device
 pub(crate) const SYSTEM_DEFAULT_INPUT: Property<AudioObjectID, System, ReadWrite, Listenable> =
-    Property::new(
-        address(
-            kAudioHardwarePropertyDefaultInputDevice,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_audio_object_id,
-        Some(encode_audio_object_id)
-    );
+Property::new(
+    address(
+        kAudioHardwarePropertyDefaultInputDevice,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_audio_object_id,
+    Some(encode_audio_object_id)
+);
 
 /// The current default output device
 pub(crate) const SYSTEM_DEFAULT_OUTPUT: Property<AudioObjectID, System, ReadWrite, Listenable> =
-    Property::new(
-        address(
-            kAudioHardwarePropertyDefaultOutputDevice,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_audio_object_id,
-        Some(encode_audio_object_id)
-    );
+Property::new(
+    address(
+        kAudioHardwarePropertyDefaultOutputDevice,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_audio_object_id,
+    Some(encode_audio_object_id)
+);
 
 /// All audio boxes known to the HAL
 pub(crate) const SYSTEM_BOX_LIST: Property<Vec<AudioObjectID>, System, ReadOnly, Listenable> =
-    Property::new(
-        address(
-            kAudioHardwarePropertyBoxList,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_vec_audio_object_id,
-        None,
-    );
+Property::new(
+    address(
+        kAudioHardwarePropertyBoxList,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_vec_audio_object_id,
+    None,
+);
 
 /// All clock devices known to the HAL
 pub(crate) const SYSTEM_CLOCK_DEVICE_LIST: Property<Vec<AudioObjectID>, System, ReadOnly, Listenable> =
-    Property::new(
-        address(
-            kAudioHardwarePropertyClockDeviceList,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_vec_audio_object_id,
-        None,
-    );
+Property::new(
+    address(
+        kAudioHardwarePropertyClockDeviceList,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_vec_audio_object_id,
+    None,
+);
 
 /// Whether the HAL is currently initialising or shutting down
 pub const SYSTEM_IS_INITING_OR_EXITING: Property<bool, System, ReadOnly, Silent> =
-    Property::new(
-        address(
-            kAudioHardwarePropertyIsInitingOrExiting,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_bool,
-        None,
-    );
+Property::new(
+    address(
+        kAudioHardwarePropertyIsInitingOrExiting,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_bool,
+    None,
+);
 
 /// Whether the system is permitted to sleep while audio is running
 pub const SYSTEM_SLEEPING_IS_ALLOWED: Property<bool, System, ReadWrite, Listenable> =
-    Property::new(
-        address(
-            kAudioHardwarePropertySleepingIsAllowed,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_bool,
-        Some(encode_bool)
-    );
+Property::new(
+    address(
+        kAudioHardwarePropertySleepingIsAllowed,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_bool,
+    Some(encode_bool)
+);
 
 /// All HAL plugins currently loaded
 pub(crate) const SYSTEM_PLUGIN_LIST: Property<Vec<AudioObjectID>, System, ReadOnly, Silent> =
-    Property::new(
-        address(
-            kAudioHardwarePropertyPlugInList,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_vec_audio_object_id,
-        None,
-    );
+Property::new(
+    address(
+        kAudioHardwarePropertyPlugInList,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_vec_audio_object_id,
+    None,
+);
 
 /// Hints to the HAL about the current power situation
 pub const SYSTEM_POWER_HINT: Property<u32, System, ReadWrite, Silent> =
-    Property::new(
-        address(
-            kAudioHardwarePropertyPowerHint,
-            kAudioObjectPropertyScopeGlobal
-        ),
-        read_u32,
-        Some(encode_u32)
-    );
+Property::new(
+    address(
+        kAudioHardwarePropertyPowerHint,
+        kAudioObjectPropertyScopeGlobal
+    ),
+    read_u32,
+    Some(encode_u32)
+);
 
 /// All audio taps known to the HAL
 pub(crate) const SYSTEM_TAP_LIST: Property<Vec<AudioObjectID>, System, ReadOnly, Listenable> =
-    Property::new(
-        address(
-            kAudioHardwarePropertyTapList,
-            kAudioObjectPropertyScopeGlobal
-        ), read_vec_audio_object_id,
-        None,
-    );
+Property::new(
+    address(
+        kAudioHardwarePropertyTapList,
+        kAudioObjectPropertyScopeGlobal
+    ), read_vec_audio_object_id,
+    None,
+);
+
+// ---- Traits ------------
+pub trait ElementSelector {
+    fn with_element(&self, element: u32) -> AudioObjectPropertyAddress;
+}
+
+impl ElementSelector for AudioObjectPropertyAddress {
+    fn with_element(&self, element: u32) -> AudioObjectPropertyAddress {
+        Self {
+            mSelector: self.mSelector,
+            mScope: self.mScope,
+            mElement: element,
+        }
+    }
+}
